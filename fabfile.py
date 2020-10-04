e929245c245e5b0d82c726f18135c315bb33a756
Enrique Arrabal <enrike9066@gmail.com>
2020-10-04 19:42:28 +0200

    step

--- fabfile.py
+++ fabfile.py
@@ -84,6 +84,7 @@ def git_pull(sha1):
 
 def app_commands():
     with cd(environments['default']['app']):
+        run(f'chmod 766 ./deployScript') # echo to environment an maintenance scripts
         run(f'./deployScript') # echo to environment an maintenance scripts
 
 def docker_commands():
